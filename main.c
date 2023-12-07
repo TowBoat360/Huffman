f185e9af4320c7272cabb0defaaed41f9ea38203
TowBoat360 <tom.ollech@gmail.com>
2023-12-07 16:29:38 +0100

    Pre-Workout

--- main.c
+++ main.c
@@ -1,48 +1,55 @@
 #include <stdio.h>
-#include "huffman.h"
-#include <string.h>
+#include "exit.h"
 
-int main()
+int main(int argc, char **argv)
 {
-	int output;
+    for (int i = 1; i < argc; i++)
+    {
+        printf("arv[%d] = %s", i, argv[i]);
+        printf("\n");
+    }
 
-	char test_input_1[] = "";
-	char test_output_1[50] = {0};
-	test_rd_chars_wr_bits(test_input_1, test_output_1);
-	output = strcmp(test_output_1, "");
-	printf("%s\n", output ? "Testfall 1 fehlgeschlagen!" : "Testfall 1 erfolgreich!");
+    for (int i = 1; i < argc; i++)
+    {
+        if (*argv[i] != '-')
+        {
 
-	char test_input_2[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxX";
-	char test_output_2[50] = {0};
-	test_rd_chars_wr_bits(test_input_2, test_output_2);
-	output = strcmp(test_output_2, "UUUUUU");
-    //printf("%s\n", test_output_2);
-	printf("%s\n", output ? "Testfall 2 fehlgeschlagen!" : "Testfall 2 erfolgreich!");
+        }
+        else
+        {
+            switch(*(argv[i]+1))
+            {
+                case 'c':
+                    printf("Compress!\n");
+                    break;
+                case 'd':
+                    printf("Decompress!\n");
+                    break;
+                case 'l': {
+                    unsigned char c = *(argv[i]+2);
+                    if (c == '\0')
+                    {
+                        printf("Level of compression: %c\n", '2');
+                    } else
+                    {
+                        printf("Level of compression: %c\n", *(argv[i]+2));
+                    }
+                }
+                    break;
+                case 'v':
+                    printf("Information about compression: \n");
+                    break;
+                case 'o':
+                    printf("Name of file: %s\n", argv[i+1]);
+                    break;
+                case 'h':
+                    printf("List of tips: \n");
+                    break;
+            }
+        }
+        //printf("i: %c\n", *argv[i]);
+        //printf("1: %c\n", *(argv[2]+1));
+    }
 
-	char test_input_3[] = "abCdefGHijKlmnOPqrStuvWXyzAbcdEF";
-	char test_output_3[50] = {0};
-	test_rd_chars_wr_bits(test_input_3, test_output_3);
-	output = strcmp(test_output_3, "####");
-    //printf("%s\n", test_output_3);
-	printf("%s\n", output ? "Testfall 3 fehlgeschlagen!" : "Testfall 3 erfolgreich!");
-
-	char test_input_4[] = "";
-	char test_output_4[50] = {0};
-	test_rd_bits_wr_chars(test_input_4, test_output_4);
-	output = strcmp(test_output_4, "");
-	printf("%s\n", output ? "Testfall 4 fehlgeschlagen!" : "Testfall 4 erfolgreich!");
-
-	char test_input_5[] = "1234567890";
-	char test_output_5[50] = {0};
-	test_rd_bits_wr_chars(test_input_5, test_output_5);
-	output = strcmp(test_output_5, "00110001001100100011001100110100001101010011011000110111001110000011100100110000");
-	printf("%s\n", output ? "Testfall 5 fehlgeschlagen!" : "Testfall 5 erfolgreich!");
-
-	char test_input_6[] = "0192837465";
-	char test_output_6[50] = {0};
-	test_rd_bits_wr_chars(test_input_6, test_output_6);
-	output = strcmp(test_output_6, "00110000001100010011100100110010001110000011001100110111001101000011011000110101");
-	printf("%s\n", output ? "Testfall 6 fehlgeschlagen!" : "Testfall 6 erfolgreich!");
-
-	return 0;
+	return EXIT_SUCCESS;
 }
