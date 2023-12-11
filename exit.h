4f0458205c0a2ae8033b7d8f71dbb8fa5df404dc
TowBoat360 <tom.ollech@gmail.com>
2023-12-11 19:59:52 +0100

    Pre-Workout 2

---
+++ exit.h
@@ -0,0 +1,16 @@
+//
+// Created by to11608 on 07.12.2023.
+//
+
+#ifndef HUFFMAN_OLD_EXIT_H
+#define HUFFMAN_OLD_EXIT_H
+
+typedef enum{
+    EXIT_SUCCESS,
+    UNKNOWN_ERROR,
+    ARGS_ERROR,
+    IO_ERROR,
+    COMPRESS_ERROR
+}CODE;
+
+#endif //HUFFMAN_OLD_EXIT_H
