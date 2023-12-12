587f494caf980719bc8ba062c3b471b79b670e6a
TowBoat360 <tom.ollech@gmail.com>
2023-12-12 13:29:19 +0100

    bool variables added

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "exit.h"
+#include <time.h>
 
 int main(int argc, char **argv)
 {
-	bool boo = false;
+	clock_t prg_start;
+	clock_t prg_end;
+
+	prg_start = clock();
+
+	bool compress = false;
+	bool decompress = false;
+
+	bool l = false;
+	char level = 2;
+
+	bool v_info = false;
+
+	bool output_file = false;
 	int index_output;
+
+	bool help = false;
 
     for (int i = 1; i < argc; i++)
     {
@@ -17,6 +33,7 @@ int main(int argc, char **argv)
 	if (argc == 1)
 	{
 		printf("\n");
+		printf("argc = 1\n");
 		printf("Fehler bei den Argumenten!\n");
 		printf("benutze -h für eine Hilfe!\n");
 
@@ -28,7 +45,7 @@ int main(int argc, char **argv)
 		{
 			if (*argv[i] != '-')
 			{
-				if (boo)
+				if (output_file)
 				{
 					if (i == index_output)
 					{
@@ -57,23 +74,26 @@ int main(int argc, char **argv)
 				switch(*(argv[i]+1))
 				{
 				case 'c':
-					printf("Compress!\n");
+					compress = true;
 					break;
 				case 'd':
-					printf("Decompress!\n");
+					decompress = true;
 					break;
 				case 'l': {
 					char c = *(argv[i]+2);
+					l = true;
+
 					if (c == '\0')
 					{
-						printf("Level of compression: %c\n", '2');
-					} else if (c < 10)
+						level = 2;
+					} else if (*(argv[i]+3) == '\0')
 					{
-						printf("Level of compression: %c\n", *(argv[i]+2));
+						level = '2';
 					}
 					else
 					{
 						printf("\n");
+						printf("zweistelliges level angegeben!\n");
 						printf("Fehler bei den Argumenten!\n");
 						printf("benutze -h für eine Hilfe!\n");
 
@@ -82,7 +102,7 @@ int main(int argc, char **argv)
 				}
 					break;
 				case 'v':
-					printf("Information about compression: \n");
+					v_info = true;
 					break;
 				case 'o':
 				{
@@ -92,12 +112,13 @@ int main(int argc, char **argv)
 						index_output = i + 1;
 						if (c != '-' && c != '\0')
 						{
-							boo = true;
+							output_file = true;
 							//printf("Name of file: %s\n", argv[i + 1]);
 						}
 						else
 						{
 							printf("\n");
+							printf("kein Name für Output-Datei vorhanden!\n");
 							printf("Fehler bei den Argumenten!\n");
 							printf("benutze -h für eine Hilfe!\n");
 
@@ -107,6 +128,7 @@ int main(int argc, char **argv)
 					else
 					{
 						printf("\n");
+						printf("kein Name für Output-Datei vorhanden!\n");
 						printf("Fehler bei den Argumenten!\n");
 						printf("benutze -h für eine Hilfe!\n");
 
@@ -115,19 +137,11 @@ int main(int argc, char **argv)
 					break;
 				}
 				case 'h':
-					printf("List of tips: \n");
-					printf("	-c --> Komprimiere\n");
-					printf("	-d --> Dekomprimiere\n");
-					printf("	-l<Level> --> Level der Kompression, wird als Zahl angegeben.\n"
-						   "		Wenn kein Level angegeben wird, wird der Standardwert 2 genommen.\n");
-					printf("	-v --> Informationen zu der Kompression\n");
-					printf("	-o <Ausgabedatei> --> Name der Ausgabedatei festlegen.\n"
-						   "		Wenn weggelassen, wird standardname '<Eingabedatei>.h bzw .d' genommen.\n");
-					printf("	<Eingabedatei>\n");
-
+					help = true;
 					break;
 				default:
 					printf("\n");
+					printf("default\n");
 					printf("Fehler bei den Argumenten!\n");
 					printf("benutze -h für eine Hilfe!\n");
 
@@ -138,8 +152,47 @@ int main(int argc, char **argv)
 			//printf("1: %c\n", *(argv[2]+1));
 		}
 
+		if (compress)
+		{
+			printf("Komprimieren!\n");
+		}
+
+		if (decompress)
+		{
+			printf("Dekomprimieren!\n");
+		}
+
+		if (l)
+		{
+			printf("Level of compression: %c\n", level);
+		}
+
+		if (help)
+		{
+			printf("List of tips: \n");
+			printf("	-c --> Komprimiere\n");
+			printf("	-d --> Dekomprimiere\n");
+			printf("	-l<Level> --> Level der Kompression, wird als Zahl angegeben.\n"
+				   "		Wenn kein Level angegeben wird, wird der Standardwert 2 genommen.\n");
+			printf("	-v --> Informationen zu der Kompression\n");
+			printf("	-o <Ausgabedatei> --> Name der Ausgabedatei festlegen.\n"
+				   "		Wenn weggelassen, wird standardname '<Eingabedatei>.h bzw .d' genommen.\n");
+			printf("	<Eingabedatei>\n");
+		}
+
 		printf("\n");
 		printf("Erfolgreich!\n");
+
+		prg_end = clock();
+
+		if (v_info)
+		{
+			printf("Ausfuehrungsstatistik\n");
+			printf(" - Groessee der Eingabedatei: \n");
+			printf(" - Groessee der Ausgabedatei: \n");
+			printf(" - Die Programmlaufzeit betrug %.2f Sekunden\n",
+					(float) (prg_end - prg_start) / CLOCKS_PER_SEC);
+		}
 
 		return EXIT_SUCCESS;
 	}
